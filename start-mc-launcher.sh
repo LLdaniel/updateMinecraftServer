960b002de3b8dd5b0f2686debdd256d9250b10d2
daniel <Daniel.adlkofer@freenet.de>
2024-05-18 16:54:45 +0200

    Start Minecraft launcher with virtual framebuffer (#2)
    * start launcher with github actions
    
    * introduce .gitignore file
    
    * formatting
    
    * formatting 2
    
    * formatting 3
    
    * formatting 4
    
    * [automated] update java server version
    
    * added DISPLAY variable
    
    * dynamically use ip for display variable
    
    * install xorg for launcher
    
    * trying virtual framebuffer for launcher
    
    * virtual framebuffer in bg
    
    * virtual framebuffer directly within script
    
    * virtual framebuffer directly with xvfb-run
    
    * [automated] update java server version
    
    * changed event to schedule (daily)

---
+++ start-mc-launcher.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+##############################################################
+# Start Minecraft Launcher to get newest java server version #
+##############################################################
+# get minecraft launcher
+wget https://launcher.mojang.com/download/Minecraft.tar.gz
+tar -xvzf Minecraft.tar.gz
+if [ -d "$HOME/.minecraft" ]
+then
+    echo -e "\e[33mClearing launcher directory...\e[0m"
+    rm -r $HOME/.minecraft
+fi
+
+# start launcher to download newest version
+timeout -s SIGKILL 20s xvfb-run ./minecraft-launcher/minecraft-launcher
+newtag=$(ls $HOME/.minecraft/versions/ | grep -E "^[0-9\.]+$")
+sha1hash=$(cat $HOME/.minecraft/versions/${newtag}/${newtag}.json | grep -Eo '"server": {"sha1": "[0-9,a-z]+"' | cut -d: -f3 | xargs)
+
+# write new version .json
+cat <<EOF > java_version_latest.json
+{
+"version":"$newtag"
+"sha1":"$sha1hash"
+}
+EOF
